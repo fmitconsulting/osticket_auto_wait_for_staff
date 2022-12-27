c5025eb29908b832ba9693370c9c9ccf5f451c2a
Marco Fontana <m.fontana@cloudsurfers.it>
2022-12-27 17:28:11 +0100

    first commit

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php return array(
+ 'id' => 'michaelvi:auto_wait_for_staff', # notrans
+ 'version' => '0.1',
+ 'name' => 'AutoWaitForStaff',
+ 'author' => 'michael@videlgauz.com',
+ 'description' => 'Automatically swithes status to "waiting for staff" upon customer\'s reply.',
+ 'url' => 'https://github.com/mvidelgauz/osticket_auto_wait_for_staff',
+ 'plugin' => 'class.AutoWaitForStaff.php:AutoWaitForStaffPlugin'
+);
